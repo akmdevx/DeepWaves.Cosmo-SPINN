6ee9436e956901d1d2211a951411f4b680be5c19
Ashutosh Kumar Mishra <mishraashu656666@gmail.com>
2026-09-17 17:59:43 +0800

    Create train.py

---
+++ Trainer/train.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import os
+import math
+import matplotlib.pyplot as plt
+
+from ./utils import util.py
+from ./Models import Evol_Net.py, Evol_Net2.py, SR_Net.py # Use according to the task
+
